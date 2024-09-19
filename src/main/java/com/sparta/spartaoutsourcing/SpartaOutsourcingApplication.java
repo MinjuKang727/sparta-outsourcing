e277b97f1d60e8a4873a2d7b2232b0820594d12a
KJG04 <freedom7113@gmail.com>
2024-09-19 11:07:33 +0900

    프로젝트 초기화

---
+++ src/main/java/com/sparta/spartaoutsourcing/SpartaOutsourcingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaoutsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaOutsourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaOutsourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartaoutsourcing/SpartaOutsourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaoutsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaOutsourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
