c8e2ae33fea8ae3cc2c8191a75e9ca9b656c4b5e
MinjuKang <minjukang727@gmail.com>
2024-09-25 08:43:00 +0900

    update : 회원 탈퇴 정보 객체 validation 수정

--- src/main/java/com/sparta/spartaoutsourcing/user/dto/request/UserDeleteRequestDto.java
+++ src/main/java/com/sparta/spartaoutsourcing/user/dto/request/UserDeleteRequestDto.java
@@ -1,6 +1,5 @@
 package com.sparta.spartaoutsourcing.user.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -9,7 +8,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserDeleteRequestDto {
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자 최소 1글자씩 포함, 길이는 8 ~ 15자 사이로 작성하여야 합니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8글자 이상, 최대 15자입니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자 최소 1글자씩 포함합니다.")
     private String password;
 
     public UserDeleteRequestDto(String password) {
