ecda7216549d5b193f71adad0da3c039390c225c
MinjuKang <minjukang727@gmail.com>
2024-09-25 08:40:23 +0900

    update : 카카오 로그인 회원 정보 객체 validation 수정

--- src/main/java/com/sparta/spartaoutsourcing/user/dto/request/KakaoUserInfoDto.java
+++ src/main/java/com/sparta/spartaoutsourcing/user/dto/request/KakaoUserInfoDto.java
@@ -1,13 +1,17 @@
 package com.sparta.spartaoutsourcing.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class KakaoUserInfoDto {
+    @NotNull
     private Long id;
     private String nickname;
+    @NotBlank(message = "이메일은 null이나 공백일 수 없습니다.")
     private String email;
 
     public KakaoUserInfoDto(Long id, String nickname, String email) {
