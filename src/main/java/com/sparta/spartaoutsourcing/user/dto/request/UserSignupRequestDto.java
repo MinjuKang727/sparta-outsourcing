23480817adf7962673900d258b5b0fd5c6139dd8
MinjuKang <minjukang727@gmail.com>
2024-09-25 08:43:21 +0900

    update : 회원 가입 정보 객체 validation 수정

--- src/main/java/com/sparta/spartaoutsourcing/user/dto/request/UserSignupRequestDto.java
+++ src/main/java/com/sparta/spartaoutsourcing/user/dto/request/UserSignupRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.spartaoutsourcing.user.dto.request;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -10,14 +11,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserSignupRequestDto {
-    @NotBlank(message = "사용자명은 null이나 공백이 아니어야 합니다.")
+    @NotBlank(message = "이름은 null이나 공백일 수 없습니다.")
     private String username;
     @Email(message = "이메일 형식이 유효하지 않습니다.")
     private String email;
-    @Size(min = 8, message = "비밀번호는 최소 8글자 이상입니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8글자 이상, 최대 15자입니다.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자 최소 1글자씩 포함합니다.")
     private String password;
+    @Nullable
     private Boolean isOwner = false;
+    @Nullable
     private String ownerKey = "";
 
     public UserSignupRequestDto(String username, String email, String password, boolean isOwner, String ownerKey) {
