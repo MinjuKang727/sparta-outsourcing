61b0041bca68db7e835e3dc00d6c1b8f80bd5917
qowlsrhks <skswlsrhks@naver.com>
2024-09-24 17:05:47 +0900

    ímain 병합

--- src/test/java/com/sparta/spartaoutsourcing/domian/store/service/StoreServiceTest.java
+++ src/test/java/com/sparta/spartaoutsourcing/domian/store/service/StoreServiceTest.java
@@ -4,9 +4,12 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.sparta.spartaoutsourcing.store.dto.store.StoreRequestDto;
+import com.sparta.spartaoutsourcing.store.exception.MaxStoreLimitException;
 import com.sparta.spartaoutsourcing.store.repository.StoreRepository;
 import com.sparta.spartaoutsourcing.store.service.StoreService;
+import com.sparta.spartaoutsourcing.user.entity.User;
 import com.sparta.spartaoutsourcing.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,7 +30,8 @@ class StoreServiceTest {
     @InjectMocks
     private StoreService storeService;
 
-    // 가게 생성 테스트
+
+    // 사장님 검증
     @Test
     void createStore_checkUser() {
         // given
@@ -41,4 +45,19 @@ class StoreServiceTest {
         assertEquals("회원이 아닙니다", exception.getMessage());
     }
 
+//    생성 가게 개수 검증
+    @Test
+    void createStore_checkStore() {
+        // given
+        Long userId = 1L;
+        StoreRequestDto requestDto = new StoreRequestDto();
+        when(userRepository.findById(userId)).thenReturn(Optional.of(new User()));
+
+        when(storeRepository.countStoreByIsCloseFalse(userId)).thenReturn(3L);
+
+        // when & then
+        MaxStoreLimitException exception = assertThrows(MaxStoreLimitException.class, ()
+                -> storeService.createStore(userId, requestDto));
+        assertEquals("가게는 3개까지 생성할 수 있습니다", exception.getMessage());
+    }
 }
