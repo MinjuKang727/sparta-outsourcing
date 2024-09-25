c3033860b416f2dc5db67ab150d19fadc18dd3eb
MinjuKang <minjukang727@gmail.com>
2024-09-25 11:59:31 +0900

    update : 로그인 되어 있지 않을때, 로그아웃 시도할 경우 처리 코드 추가

--- src/main/java/com/sparta/spartaoutsourcing/auth/UserLogoutHandler.java
+++ src/main/java/com/sparta/spartaoutsourcing/auth/UserLogoutHandler.java
@@ -6,10 +6,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-
-import java.io.IOException;
 
 @Slf4j(topic = "UserLogoutHandler")
 public class UserLogoutHandler implements LogoutHandler {
@@ -25,17 +24,15 @@ public class UserLogoutHandler implements LogoutHandler {
         log.info("로그아웃 시도");
         try {
             String token = request.getHeader(JwtUtil.AUTHORIZATION_HEADER);
+            if (token == null) {
+                log.error("로그인 되어 있지않습니다.");
+                return;
+            }
             this.tokenBlacklistService.addTokenToBlackList(token);
             response.setHeader(JwtUtil.AUTHORIZATION_HEADER, null);
         } catch (ServletException e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            try {
-                response.getWriter().write(e.getMessage());
-            } catch (IOException ex) {
-                throw new RuntimeException(e);
-            }
+            log.error("Logout Failed");
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
         }
-
-
     }
 }
